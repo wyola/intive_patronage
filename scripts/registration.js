16f4afcd8d81e881c3eaabdf32c7038871da6256
wy_ola <80450315+wyola@users.noreply.github.com>
2023-01-17 20:22:56 +0100

    form validation, displaying and clearing errors

---
+++ scripts/registration.js
@@ -0,0 +1,80 @@
+const form = document.getElementsByTagName('form')[0];
+form.addEventListener("submit", handleSubmit);
+
+for (const input of form.elements) {
+    input.addEventListener('keydown', clearError);
+}
+
+function clearError(event) {
+    const input = event.target;
+
+    if (input.classList.contains('invalid')) {
+        input.classList.remove('invalid');
+        input.parentNode.nextSibling.remove();
+    } 
+}
+
+function displayError(input, errorMessage) {
+    const error = document.createElement('div');
+    error.classList.add('error-message');
+    error.innerText = errorMessage;
+    input.parentNode.after(error);
+
+    input.classList.add('invalid');
+}
+
+let users = [
+    {
+        user: 'wyola123',
+        email: 'wyola@gmail.com',
+        password: 'Test123'
+    },
+    {
+        user: 'michal123',
+        email: 'michal@gmail.com',
+        password: 'Michal123'
+    },
+    {
+        user: 'rico123',
+        email: 'rico@gmail.com',
+        password: 'Rico123'
+    },
+]
+
+
+function validateForm() {
+    let isFormValid = true;
+
+    for(const user of users) {
+        if(user.email === form.elements.email.value) {
+            displayError(form.elements.email, 'istnieje już użytkownik z takim adresem email')
+            isFormValid = false;
+        }
+
+        if(user.user === form.elements.userName.value) {
+            displayError(form.elements.userName, 'istnieje już użytkownik o takiej nazwie')
+            isFormValid = false;
+        }
+    }
+
+    if (form.elements.email.value !== form.elements.confirmEmail.value) {
+        displayError(form.elements.confirmEmail, 'wpisz ten sam email co powyżej')
+        isFormValid = false;
+    }
+
+    return isFormValid;
+}
+
+function registerUser() {
+    // JSON.stringify()
+    // JSON.parse()
+}
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log('test');
+    const {userName, password, email, confirmEmail} = event.target.elements;
+
+    validateForm();
+}
