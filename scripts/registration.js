012bf9a8019e2ce4ecb6d87515d85da7b1220157
wy_ola <80450315+wyola@users.noreply.github.com>
2023-01-19 18:37:58 +0100

    username extra validation

--- scripts/registration.js
+++ scripts/registration.js
@@ -12,21 +12,33 @@ function validateForm() {
 
     for(const user of users) {
         if(user.email === form.elements.email.value) {
-            displayMessage(form.elements.email, 'istnieje już użytkownik z takim adresem email')
+            displayMessage(form.elements.email, 'istnieje już użytkownik z takim adresem email');
             isFormValid = false;
         }
 
         if(user.user === form.elements.userName.value) {
-            displayMessage(form.elements.userName, 'istnieje już użytkownik o takiej nazwie')
+            displayMessage(form.elements.userName, 'istnieje już użytkownik o takiej nazwie');
             isFormValid = false;
         }
     }
 
     if (form.elements.email.value !== form.elements.confirmEmail.value) {
-        displayMessage(form.elements.confirmEmail, 'adresy email muszą być takie same')
+        displayMessage(form.elements.confirmEmail, 'adresy email muszą być takie same');
         isFormValid = false;
     }
 
+    //username validation - at least 1 digit
+    if(!form.elements.userName.value.match(/\d/)) {
+        displayMessage(form.elements.userName, 'nazwa użytkownika powinna zawierać co najmniej jedną cyfrę');
+        isFormValid = false;
+    }
+
+    //username validation - at least 5 letters
+    const userNameLetters = form.elements.userName.value.replace(/[a-zA-Z]+/g, '');
+    if(userNameLetters.length < 5) {
+        displayMessage(form.elements.userName, 'nazwa użytkownika powinna zawierać co najmniej 5 liter');
+        isFormValid = false;
+    }
     return isFormValid;
 }
 
